105bf3a02cec5c7ca8acca3cecd932e2aa2b87ba
Nafiz Sadman <nafizsadman420@gmail.com>
2018-12-05 06:20:57 +0000

    setup
    Co-authored-by: AkibSadmanee <akibsadmanee@gmail.com>
    Co-authored-by: Samin Bin karim <saminbinkarim@gmail.com>
    Co-authored-by: Touseef Aziz Khan <touseef.aziz@gmail.com>

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
-//########### Import packages #############
+//########### Import packages & Declare Variables #############
 var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var db_url = "mongodb://"+process.env.IP+":27017";
 
 //Init App
 var app = express();
 //Set up server
 var server = http.Server(app);
 
-//############# Middlewares ################
+
+//##################### Middlewares ##########################
+//Connect mongoose
+mongoose.connect(db_url+"/chats");
+mongoose.connection.on('error', function(error){
+  console.log('Could not connect to MongoDB');
+});
 //body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+//get static files
+app.use(express.static(__dirname + '/public'));
 
-//############### Routes ###################
+
+//######################## Routes ############################
 app.get('/', function(req,res){
     //EJS Render GOES HERE
   res.send('OK');
